37ff0f58f7ac63dffa1221adce6fa9d1e4e79f13
Tinuade Adeleke <summittinuade@gmail.com>
2017-10-28 13:56:46 +0100

    update

--- google_places.py
+++ google_places.py
@@ -1,21 +1,18 @@
 import json
 import requests
 
-api_key = 'AIzaSyBCvwxLoAmIwGKBsOTtuQIJ4dpVuv2ALPk'
+# api key - AIzaSyBCvwxLoAmIwGKBsOTtuQIJ4dpVuv2ALPk
 
+search_term = 'attractions+in+Lagos'
 
-def get_attractions(place):
-	search_term = 'attractions+in+'+ place
-	url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + search_term + '&key=' + api_key + '&type=point_of_interest'
-	r = requests.get(url)
-	response = json.loads(r.text)
-	result = ''
-	locations = response['results']
-	for location in locations:
-		name = location['name']
-		address = location['formatted_address']
-		result += name + ', ' + address + '. '
-	return result
+r = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + search_term + '&key=AIzaSyBCvwxLoAmIwGKBsOTtuQIJ4dpVuv2ALPk&type=point_of_interest')
+response = json.loads(r.text)
+result = []
+locations = response['result']
+for location in locations:
+	name = location['name']
+	address = location['formatted_address']
+	result.append((name, address))
+for item in result:
+	print(item)
 
-if __name__ == '__main__':
-	print(get_attractions('Ogbomosho'))
